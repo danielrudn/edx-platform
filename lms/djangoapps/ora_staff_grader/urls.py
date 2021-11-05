12a2f6d2a31094c09782f10ab673da9ad6832c2c
Nathan Sprenkle <nsprenkle@edx.org>
2021-11-05 15:42:29 -0400

    feat: add submission lock path to URLs

--- lms/djangoapps/ora_staff_grader/urls.py
+++ lms/djangoapps/ora_staff_grader/urls.py
@@ -4,7 +4,7 @@ URLs for Enhanced Staff Grader (ESG) backend-for-frontend (BFF)
 
 from django.urls.conf import path
 
-from lms.djangoapps.ora_staff_grader.views import InitializeView
+from lms.djangoapps.ora_staff_grader.views import InitializeView, SubmissionLockView
 
 
 urlpatterns = []
@@ -12,5 +12,8 @@ urlpatterns = []
 urlpatterns += [
     path(
         'initialize', InitializeView.as_view(), name='initialize'
+    ),
+    path(
+        'submission/lock', SubmissionLockView.as_view(), name='lock'
     ),
 ]
